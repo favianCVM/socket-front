ae94cfb2363698f9256cf59903bb249afd4a6950
Fabian <fabiancvm7@gmail.com>
2022-02-12 23:14:47 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useEffect } from "react";
+import io from "socket.io-client";
+import "./App.css";
+const ENDPOINT = "http://localhost:5000";
+
+const socket = io.connect(ENDPOINT);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [response, setResponse] = useState("NONE");
+
+	useEffect(() => {
+		socket.on("message", (data) => {
+			console.log("data ::: ", data);
+			setResponse(data);
+		});
+
+		// CLEAN UP THE EFFECT
+		return () => socket.disconnect();
+
+		// testEndpoint();
+	}, []);
+
+	// const testEndpoint = async () =>
+	// 	fetch("http://localhost:5000/api/v1/tests/")
+	// 		.then((data) => data.json())
+	// 		.then((data) => console.log(data))
+	// 		.catch((err) => console.log(err));
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>RESPONSE::: {response}</p>
+
+				<button
+					onClick={() => {
+						socket.emit("message1", "aver");
+					}}
+				>
+					emitir
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
